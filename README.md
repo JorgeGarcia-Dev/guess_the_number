e3b3125ad2f02060fd01fbe7cf3b9d416f3d3fe9
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2023-06-21 18:56:11 -0600

    First Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+!['Banner Python'](img/Banner_Python.png)
+
+# "Guess the number"
+#### This is a game developed in Python.
+
+---
+
+* The game is simple, the computer will choose a number between 1 and 100 depend of you selected the level (1 -> 10) and you have to guess it.
+
+---
+
+* The game will give you clues to help you guess the number, indicate you if is less or greater.
+
+---
+
+* If you want to play, you can download the file "main.py" and run it on your computer.
+
+---
+
+* For run the game, you need to open a terminal and write:
+
+  * ```python main.py``` (Windows)
+  * ```python3 main.py``` (Mac OS, Linux)
+
+* (You need a Python 3.6 or higher version.)
+
+---
+
+#### Have fun!
---
+++ main.py
@@ -0,0 +1,54 @@
+"""
+Create a game "Guess the number" with Python.
+"""
+
+import random
+
+
+def validate_number(message):
+    while True:
+        try:
+            data = int(input("Enter " + message))
+            return data
+        except ValueError:
+            print("The character must be an integer.")
+
+
+def guess_the_number():
+    print("Welcome to the game 'Guess the number'... Are you ready?\n")
+
+    level_of_difficulty = validate_number(
+        "the level of difficulty: 1 = easy, 10 = hard: "
+    )
+
+    selected_difficulty_level = int(f"{level_of_difficulty}0")
+
+    print(
+        "You must find the secret number "
+        f"between 1 & {selected_difficulty_level}."
+    )
+
+    loop = 1
+
+    secret_number = random.randint(1, selected_difficulty_level)
+
+    while loop <= 5:
+        player_number = validate_number("a number: ")
+
+        if secret_number > player_number:
+            loop += 1
+            print(f"The number is greater than {player_number}.")
+
+        elif secret_number < player_number:
+            loop += 1
+            print(f"The number is less than {player_number}.")
+
+        else:
+            print(f"Congratulations,you guessed the number {secret_number} !!")
+            break
+
+    if loop > 5:
+        print(f"You lost !! The secret number was {secret_number}.")
+
+
+guess_the_number()
