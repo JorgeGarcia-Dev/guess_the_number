39d07f0806c9758caa584866f043a7490d3502a4
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2023-07-07 17:05:38 -0600

    Se modificaron las validaciones, asignando una para el input del nivel de dificultad, en el que se vrifica que el valor sea de tipo entero, y se encuentre dentro del rango de 1 a 10, y se agregó una segunda valodación para la iteración del input del usuario dentro del programa que valida si el valor ingresado es de tipo entero.

--- main.py
+++ main.py
@@ -5,7 +5,96 @@ Create a game "Guess the number" with Python.
 import random
 
 
-def validate_number(message):
+def validate_type_and_range_level_of_dificultty(message):
+    """
+    This function validates the type and range of numbers.
+
+    Parameters:
+    message (str): The message to display to the user.
+
+    Returns:
+    int: The validated number.
+
+    Examples:
+    >>> validate_type_and_range_level_of_dificultty(
+        "Enter the level of difficulty: 1 = easy, 10 = hard: "
+        )
+    Enter the level of difficulty: 1 = easy, 10 = hard: 10
+    10
+
+    >>> validate_type_and_range_level_of_dificultty(
+        "Enter the level of difficulty: 1 = easy, 10 = hard: "
+        )
+    Enter the level of difficulty: 1 = easy, 10 = hard: 5
+    5
+
+    >>> validate_type_and_range_level_of_dificultty(
+        "Enter the level of difficulty: 1 = easy, 10 = hard: "
+        )
+    Enter the level of difficulty: 1 = easy, 10 = hard: "10"
+    ValueError: invalid literal for int() with base 10: '10'
+
+    >>> validate_type_and_range_level_of_dificultty(
+        "Enter the level of difficulty: 1 = easy, 10 = hard: "
+        )
+    Enter the level of difficulty: 1 = easy, 10 = hard: diez
+    ValueError: invalid literal for int() with base 10: 'diez'
+
+    >>> validate_type_and_range_level_of_dificultty(
+        "Enter the level of difficulty: 1 = easy, 10 = hard: "
+        )
+    Enter the level of difficulty: 1 = easy, 10 = hard: 20
+    The number must be between 1 and 10.
+    """
+    while True:
+        try:
+            data = int(input("Enter " + message))
+            if data >= 1 and data <= 10:
+                return data
+            else:
+                print("The number must be between 1 and 10.")
+        except ValueError:
+            print("The character must be an integer.")
+
+        except TypeError as error:
+            print(error)
+
+
+def validate_type_number(message: str) -> int:
+    """
+    This function validates the type and range of numbers.
+
+    Parameters:
+    message (str): The message to display to the user.
+
+    Returns:
+    int: The validated number.
+
+    Examples:
+    >>> validate_typ__number(
+        "Enter a number: "
+        )
+    Enter a number: 10
+    10
+
+    >>> validate_type_number(
+        "Enter a number: "
+        )
+    Enter a number: 5
+    5
+
+    >>> validate_type_number(
+        "Enter a number: "
+        )
+    Enter a number: "10"
+    ValueError: invalid literal for int() with base 10: '10'
+
+    >>> validate_type_number(
+        "Enter a number: "
+        )
+    Enter a number: diez
+    ValueError: invalid literal for int() with base 10: 'diez'
+    """
     while True:
         try:
             data = int(input("Enter " + message))
@@ -13,19 +102,52 @@ def validate_number(message):
         except ValueError:
             print("The character must be an integer.")
 
+        except TypeError as error:
+            print(error)
+
 
 def guess_the_number():
+    """
+    This function implements the 'Guess the Number' game.
+
+    The game prompts the user to guess a secret number within
+    a given range of difficulty levels.
+    The user has 5 attempts to guess the number. After each guess,
+    the game provides feedback on whether the secret number is greater
+    or less than the guess.
+    If the user guesses the number correctly within the allowed attempts,
+    they win the game. Otherwise, they lose.
+
+    Parameters:
+    message (str): The message to display to the user.
+
+    Returns:
+    message (str): Response message depending on whether the number is higher or lower,
+    if the user has guessed the number, or if his chances have been exhausted.
+
+    Example:
+    >>> guess_the_number()
+    Welcome to the game 'Guess the number'... Are you ready?
+
+    Enter the level of difficulty: 1 = easy, 10 = hard: 5
+    You must find the secret number between 1 & 50.
+    Guess a number: 25
+    The number is greater than 25.
+    Guess a number: 40
+    The number is less than 40.
+    Guess a number: 35
+    Congratulations, you guessed the number! (The secret number was 35)
+    """
     print("Welcome to the game 'Guess the number'... Are you ready?\n")
 
-    level_of_difficulty = validate_number(
+    level_of_difficulty = validate_type_and_range_level_of_dificultty(
         "the level of difficulty: 1 = easy, 10 = hard: "
     )
 
     selected_difficulty_level = int(f"{level_of_difficulty}0")
 
     print(
-        "You must find the secret number "
-        f"between 1 & {selected_difficulty_level}."
+        "You must find the secret number " f"between 1 & {selected_difficulty_level}."
     )
 
     loop = 1
@@ -33,7 +155,7 @@ def guess_the_number():
     secret_number = random.randint(1, selected_difficulty_level)
 
     while loop <= 5:
-        player_number = validate_number("a number: ")
+        player_number = validate_type_number("a number: ")
 
         if secret_number > player_number:
             loop += 1
